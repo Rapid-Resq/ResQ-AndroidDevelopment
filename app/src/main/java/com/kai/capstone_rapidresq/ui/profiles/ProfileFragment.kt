5f3ded18af3cf4fae0ceb0322870760bcf61927a
Kai <142235179+xendsa@users.noreply.github.com>
2023-12-04 23:29:32 +0700

    Initial commit

--- app/src/main/java/com/kai/capstone_rapidresq/ui/profiles/ProfileFragment.kt
+++ app/src/main/java/com/kai/capstone_rapidresq/ui/profiles/ProfileFragment.kt
@@ -20,7 +20,7 @@ class ProfileFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentProfileBinding.inflate(inflater, container, false)
         return binding.root
     }
